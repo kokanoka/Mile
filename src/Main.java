61ad43739498c025c0047dc8efd63c0ba8ceb94a
kokanoka <kokanoka30@gmail.com>
2021-08-13 16:43:21 +0300

    добавила код для проверки суммы бонуса, файл .gitignore

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 12674;
+        int mile = 20;
+        int bonus = price / mile;
+        System.out.println(bonus);
+    }
+}
